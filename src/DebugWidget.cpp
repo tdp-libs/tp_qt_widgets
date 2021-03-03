11ab3e6639b5b0386d8b97fac6ee12c902db54ae
Tom Paynter <tompaynter@tdpe.co.uk>
2021-03-03 12:50:53 +0000

    Made the build compatible with Qt5 and Qt6.

--- src/DebugWidget.cpp
+++ src/DebugWidget.cpp
@@ -1,6 +1,9 @@
 #include "tp_qt_widgets/DebugWidget.h"
 
+#include "tp_qt_utils/CrossThreadCallback.h"
+
 #include "tp_utils/DebugUtils.h"
+#include "tp_utils/MutexUtils.h"
 
 namespace tp_qt_widgets
 {
@@ -10,17 +13,31 @@ struct DebugWidget::Private
 {
   DebugWidget* q;
 
+  TPMutex mutex{TPM};
+  std::string messages;
+
   //################################################################################################
   Private(DebugWidget* q_):
     q(q_)
   {
+    // This is not thread safe but we get away with it most of the time...
     debugCallback.connect(tp_utils::DBG::Manager::instance().debugCallbacks);
   }
 
   //################################################################################################
+  tp_qt_utils::CrossThreadCallback addMessages = std::function<void()>([&]
+  {
+    TP_MUTEX_LOCKER(mutex);
+    q->setPlainText(q->toPlainText() + QString::fromStdString(messages));
+    messages.clear();
+  });
+
+  //################################################################################################
   tp_utils::Callback<void(tp_utils::MessageType, const std::string&)> debugCallback = [&](tp_utils::MessageType, const std::string& msg)
   {
-    q->setPlainText(q->toPlainText() + QString::fromStdString(msg));
+    TP_MUTEX_LOCKER(mutex);
+    messages+=msg;
+    addMessages.call();
   };
 };
 
