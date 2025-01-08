bd389ca0f2c1bf096005eec4b09d416581f1a209
trinam <trinam@omi.so>
2025-01-08 23:01:21 +0100

    ENGI-435-LightMask-in-progress

--- src/BlockingOperationDialog.cpp
+++ src/BlockingOperationDialog.cpp
@@ -104,7 +104,7 @@ struct BlockingOperationDialog::Private
 BlockingOperationDialog::BlockingOperationDialog(const std::function<bool()>& poll,
                                                  const QString& windowTitle,
                                                  QWidget* parent):
-  QDialog(parent),
+  QDialog(parent, Qt::Window),
   d(new Private(poll, windowTitle))
 {
   setWindowTitle(windowTitle);
